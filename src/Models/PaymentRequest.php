8f7c49cfb59d4540a94711f5ca684beed80f8b86
stephenmudere <stephenmudere@users.noreply.github.com>
2021-03-19 05:47:48 +0000

    Fix styling

--- src/Models/PaymentRequest.php
+++ src/Models/PaymentRequest.php
@@ -8,13 +8,7 @@ use Illuminate\Database\Eloquent\Model;
  */
 class PaymentRequest extends Model
 {
-
     protected $fillable = [
-        'user_id', 'customer_id', 'amount','trip_id','webhook_status','reference_number','transaction_id','token','payment_id','currency','currency_symbol','result_code','result_description'
+        'user_id', 'customer_id', 'amount','trip_id','webhook_status','reference_number','transaction_id','token','payment_id','currency','currency_symbol','result_code','result_description',
     ];
-	
-
 }
-
-
- ?>
